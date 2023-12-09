48c2304cdb8172739548cf0a6d7569181bb15bc7
azer <benazzouzaze@gmail.com>
2023-12-09 21:06:50 +0100

    🚀🔥

--- src/app/shared/home/carousel-categorie/carousel-categorie.component.ts
+++ src/app/shared/home/carousel-categorie/carousel-categorie.component.ts
@@ -16,21 +16,22 @@ export class CarouselCategorieComponent {
       id: 1,
       name: 'Categorie 1',
       description: 'Description 1',
-      image: 'https://via.placeholder.com/350x150'
+      image: 'https://via.placeholder.com/1280x450'
     },
     {
       id: 2,
       name: 'Categorie 2',
       description: 'Description 2',
-      image: 'https://via.placeholder.com/350x150'
+      image: 'https://via.placeholder.com/1280x450'
     },
     {
       id: 3,
       name: 'Categorie 3',
       description: 'Description 3',
-      image: 'https://via.placeholder.com/350x150'
+      image: 'https://via.placeholder.com/1280x450'
     }
   ];
+  
   constructor() {}
 
   ngOnInit() {}
