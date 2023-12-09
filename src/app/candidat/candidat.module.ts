ee9c9db1b1122353238037ac8974db1abfe5e9b5
azer <benazzouzaze@gmail.com>
2023-12-09 14:27:37 +0100

    ❤😇

--- src/app/candidat/candidat.module.ts
+++ src/app/candidat/candidat.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CandidatRoutingModule } from './candidat-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    CandidatRoutingModule
   ]
 })
 export class CandidatModule { }
