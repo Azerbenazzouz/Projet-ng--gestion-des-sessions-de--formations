bf456351fa534f7fe5f0df898e9d05035f71dd26
azer <benazzouzaze@gmail.com>
2023-12-18 21:39:54 +0100

    Add authentication check in details component

--- src/app/shared/details/details.component.ts
+++ src/app/shared/details/details.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [NavbarComponent , FooterComponent],
+  imports: [NavbarComponent , FooterComponent , CommonModule ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-
+  constructor( private route : Router) { }
+  
+  Auth = ()=>{
+    return window.localStorage.getItem("accessToken") ? true : false;
+  }
+  
 }
