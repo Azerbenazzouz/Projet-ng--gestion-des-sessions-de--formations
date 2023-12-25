4a092a07f863d4d0bdb9993cb696fe9c25d9914e
azer <benazzouzaze@gmail.com>
2023-12-25 23:30:17 +0100

    Add CommonModule import to FormationInscritComponent

--- src/app/candidat/formation-inscrit/formation-inscrit.component.ts
+++ src/app/candidat/formation-inscrit/formation-inscrit.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-formation-inscrit',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,CommonModule],
   templateUrl: './formation-inscrit.component.html',
   styleUrl: './formation-inscrit.component.css'
 })
 export class FormationInscritComponent {
-
+  formations : any[] = [];
 }
