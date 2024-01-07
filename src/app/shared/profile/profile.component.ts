e1794b9f0049383dc0282ea62d138a9eea848438
azer <benazzouzaze@gmail.com>
2024-01-07 13:23:31 +0100

    Update profile and settings components

--- src/app/shared/profile/profile.component.ts
+++ src/app/shared/profile/profile.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute , Router, RouterLink } from '@angular/router';
+import { Client } from '../model/client';
+import { ClientService } from '../service/client.service';
 
 @Component({
   selector: 'app-profile',
@@ -12,5 +14,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  client? : Client;
+  id? : number;
 
+  constructor(private route : ActivatedRoute, private router : Router ,private clientServes : ClientService) {}
+
+  ngOnInit(): void {
+    this.id = Number(localStorage.getItem("id"));
+
+    this.clientServes.getUSerById2(this.id).subscribe({
+      next : (data : any) => {
+        this.client = data;
+        console.log(this.client);
+      }
+    })
+    
+  }
 }
--- src/app/shared/settings/settings.component.ts
+++ src/app/shared/settings/settings.component.ts
@@ -1,15 +1,76 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { CommonModule } from '@angular/common';
+
+import { FormControl , FormGroup , ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { ClientService } from '../service/client.service';
+import { Client } from '../model/client';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [FooterComponent,NavbarComponent,CommonModule],
+  imports: [FooterComponent,NavbarComponent,CommonModule,ReactiveFormsModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.css'
 })
 export class SettingsComponent {
+  client? : Client;
+  id? : number;
+  constructor(private clientService : ClientService) { }
+
+  clientForm = new FormGroup({
+    name : new FormControl('',Validators.required),
+    cin: new FormControl('',{validators: [Validators.required, Validators.minLength(8), Validators.maxLength(8) , Validators.pattern("^[0-9]*$")]}),
+    telephone: new FormControl('',{validators: [Validators.required, Validators.minLength(8), Validators.maxLength(8) , Validators.pattern("^[0-9]*$")]}),
+    email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+    role: new FormControl('',Validators.required),
+    img: new FormControl('',Validators.nullValidator)
+  });
+
+  MdpChangeForm = new FormGroup({
+    password: new FormControl('',Validators.required),
+    newPassword: new FormControl('',Validators.required),
+    newPassword2: new FormControl('',Validators.required)
+  });
+
+  EmailChangerForm = new FormGroup({
+    email: new FormControl('', {validators: [Validators.required, Validators.email]}),
+    password: new FormControl('',Validators.required)
+  });
+
+  ngOnInit() {
+    this.id = Number(localStorage.getItem('id'));
+
+    this.clientService.getUSerById2(this.id).subscribe({
+      next : (data : any) => {
+        this.client = data;
+        console.log(this.clientForm.value);
+        this.clientForm.patchValue({
+          name : this.client?.name??'',
+          cin : this.client?.cin??'',
+          telephone : this.client?.telephone??'',
+          email : this.client?.email??'',
+          role : this.client?.role??'',
+          img : this.client?.img??''
+        });
+      }
+    })
+
+  }
+
+  changerInformation(){
+    console.log(this.clientForm.value);
+  }
+
+  changerPassword(){
+    console.log(this.MdpChangeForm.value);
+  }
+
+  changerEmail(){
+    console.log(this.EmailChangerForm.value);
+  }
 
 }
